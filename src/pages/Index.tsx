a6276483cdf4027f5cef7250624b74e3aeababae
Wesley Sun <wqzsun@gmail.com>
2025-05-01 15:31:21 -0400

    init

---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+// Update this page (the content is just a fallback if you fail to update the page)
+
+const Index = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+      </div>
+    </div>
+  );
+};
+
+export default Index;
